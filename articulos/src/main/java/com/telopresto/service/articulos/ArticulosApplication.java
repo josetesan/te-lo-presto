a7cf552f9aaf1145065223faf8ced1a672e3da91
jose luis sanchez romero <muallin@gmail.com>
2018-02-24 17:59:29 +0100

    Te Lo Presto 0.0.1

---
+++ articulos/src/main/java/com/telopresto/service/articulos/ArticulosApplication.java
@@ -0,0 +1,12 @@
+package com.telopresto.service.articulos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticulosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticulosApplication.class, args);
+	}
+}
---
+++ articulos/src/test/java/com/telopresto/service/articulos/ArticulosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.telopresto.service.articulos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArticulosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
