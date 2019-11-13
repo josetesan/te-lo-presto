9aeafaf56bd177a7507356389f25f8443d001844
Jose Luis Sanchez <muallin@gmail.com>
2019-11-13 12:56:56 +0100

    Updated to boot 2.2.1 and cloud hoxton.rc2

--- gateway/src/main/java/com/telopresto/service/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/telopresto/service/gateway/GatewayApplication.java
@@ -2,13 +2,23 @@ package com.telopresto.service.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableZuulProxy
 public class GatewayApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes().build();
+	}
+
+
+
 }
